c4cd0c415c075991de4d784845363cbba1241c14
Roy Storey <kiwiroy@users.noreply.github.com>
2019-07-05 12:12:30 +1200

    Error in xo[[1]] : subscript out of bounds

---
+++ tests/testthat/test-90-regression.R
@@ -0,0 +1,6 @@
+context("regression tests")
+
+test_that("regression - handle NULL from knitr::opts_chunk$get('engine.opts')$perl ", {
+  words <- perlbrewr:::shellwords(x = NULL)
+  expect_equal(words, character(0))
+})
